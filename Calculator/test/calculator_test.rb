bbb64f01d0d2e8a921f427114a2db98b2c1f1113
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-01-29 20:24:37 -0600

    refactoring to test_adition

--- Calculator/test/calculator_test.rb
+++ Calculator/test/calculator_test.rb
@@ -4,12 +4,13 @@ require 'calculator'
 
 class TestCalculator < MiniTest::Unit::TestCase
   
-  def test_adition
-    assert_equal 4, Calculator.new.add(2,2)
-  end  
-
-  def test_adition_dos
-    assert_equal 6, Calculator.new.add(4,2)
+  def setup
+    @cal = Calculator.new 
   end
+
+  def test_adition
+    assert_equal 4, @cal.add(2,2)
+    assert_equal 6, @cal.add(4,2)
+  end  
 
 end
